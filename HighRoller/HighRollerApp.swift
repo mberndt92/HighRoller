9a9c6c42bd342444ba98966c25f6fea53edadc1b
Mberndt92 <maximilian_berndt@icloud.com>
2023-05-08 10:18:12 +0900

    Initial Commit

---
+++ HighRoller/HighRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollerApp.swift
+//  HighRoller
+//
+//  Created by Maximilian Berndt on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct HighRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
