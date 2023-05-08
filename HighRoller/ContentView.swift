dabb58152393c86c3b12af5b9caa315060b39120
Mberndt92 <maximilian_berndt@icloud.com>
2023-05-08 10:35:46 +0900

    MVP kind of

--- HighRoller/ContentView.swift
+++ HighRoller/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var results: [Int] = []
+    
+    @State private var currentResult: Int = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                VStack(spacing: 15) {
+                    Image(systemName: "die.face.\(currentResult)")
+                        .resizable()
+                        .frame(width: 64, height: 64)
+                    
+                    Button("Roll dice", action: rollDice)
+                        .padding()
+                        .frame(maxWidth: 150)
+                        .font(.headline)
+                        .background(.regularMaterial)
+                        .cornerRadius(25)
+                        .shadow(radius: 10)
+                }
+                .padding()
+            }
+            .toolbar {
+                ToolbarItem {
+                    Image(systemName: "clock.arrow.circlepath")
+                }
+            }
         }
-        .padding()
+    }
+    
+    private func rollDice() {
+        withAnimation {
+            currentResult = Int.random(in: 1...6)
+        }
+        results.append(currentResult)
     }
 }
 
